11aab5568db16bbc09eea57d03893ec6a3eb4409
Jrew <jrewmohamed@gmail.com>
2019-01-24 15:46:20 -0500

    added homePage

--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,36 @@ import React, { Component } from "react";
 import "./App.css";
 import Img from './components/imgComponent/Img'
 import Axios from "axios";
+import homePage from "./pages/homePage";
+
 class App extends Component {
 
   state = {
     img: []
   }
-//loads content to page
+  //loads content to page
   componentDidMount() {
     Axios.get('/data')
       .then(image => {
-        if(image.data === null){
+        if (image.data === null) {
           return
-        }else{
+        } else {
           const img = image.data
           this.setState({ img })
         }
       })
   }
-//renders images to page
+  //renders images to page
   render() {
     return (
       <div>
-        {this.state.img.map((image,i) => {
+        <homePage />
+        {/* {this.state.img.map((image, i) => {
           //only renders 4 videos
-          if(i <=3) {
-            return image ? <Img key={i+1} src={image} num={i+1}/> : null
-          }else return null 
-        })}
+          if (i <= 3) {
+            return image ? <Img key={i + 1} src={image} num={i + 1} /> : null
+          } else return null
+        })} */}
       </div>
     );
   }
---
+++ client/src/pages/homePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class homePage extends Component {
+    
+    render() {
+
+        return (
+            <div>
+                <h1>Eagle Eye Analytics</h1>
+            </div>
+        )
+    }
+
+}
+
+export default homePage;

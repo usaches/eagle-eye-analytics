20a291d742636f7c06bac5ba4db96730a7c61776
Jeremy Swain <Jatlanta3000@gmail.com>
2019-01-23 19:36:29 -0500

    update9

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get('/data', (req, resp) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+//server up and running
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);

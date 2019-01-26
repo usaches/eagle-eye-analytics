ba191da984f46e3f096d037c76f2a150bfddc622
Jrew <jrewmohamed@gmail.com>
2019-01-26 10:52:49 -0500

    added react-materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import {Navbar, NavItem} from "react-materialize";
 import "./App.css";
 import Img from './components/imgComponent/Img'
 import Axios from "axios";
-import homePage from "./pages/homePage";
+// import homePage from "./pages/homePage";
 
 class App extends Component {
 
@@ -25,14 +26,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <homePage />
-        {/* {this.state.img.map((image, i) => {
+
+        <Navbar brand='logo' right>
+          <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+          <NavItem href='components.html'>Components</NavItem>
+        </Navbar>
+
+        {this.state.img.map((image, i) => {
           //only renders 4 videos
           if (i <= 3) {
             return image ? <Img key={i + 1} src={image} num={i + 1} /> : null
           } else return null
-        })} */}
+        })}
+
       </div>
+
     );
   }
 }
